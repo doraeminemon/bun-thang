724f165a69725855e4930d4ed2c8354eaeb516d2
Tyson Do <doraeminemon@gmail.com>
2025-03-04 13:49:27 +0700

    Add image optimization and github workflow

---
+++ src/components/image.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { optimize } from '$lib/image';
+  // import type { Photo } from '$lib/types';
+
+  export let photo;
+</script>
+
+<img
+  class="absolute left-0 top-0 w-full h-full"
+  srcset={optimize(photo.url)}
+  alt={photo.description}
+/>
--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,14 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			images: {
+				sizes: [640, 828, 1200, 1920, 3840],
+				formats: ['image/avif', 'image/webp'],
+				minimumCacheTTL: 300,
+				domains: ['bun-thang.vercel.app'],
+			}
+		})
 	},
 
 	preprocess: [mdsvex()],
