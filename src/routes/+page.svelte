a43924a96f5e7500a117198fdbd9d2ba7b704bdb
Tyson Do <doraeminemon@gmail.com>
2025-03-04 16:45:58 +0700

    Update title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <svelte:head>
 	<title>Thuốc Lào Blog</title>
-	<meta name="description" content="Một trang web viết về văn hoá Hà Nội" />
+	<meta name="description" content="Khoảnh đất nơi góp nhặt những ý nghĩ về văn hoá Hà Nội" />
 </svelte:head>
 <script>
 	import ThuocLaoImg from '$lib/assets/img/thuoc-lao.jpg';
@@ -8,7 +8,7 @@
 <section class="m-4 min-h-screen flex flex-col">
   <h1>Thuốc Lào Blog</h1>
   <img src={ThuocLaoImg} width="" alt="Some green tea with smoking pipe">
-  <p class="py-4">Chỉ đơn giản là một trang blog về văn hoá Việt Nam</p>
+  <p class="py-4">Khoảnh đất nơi góp nhặt những ý nghĩ về văn hoá Hà Nội</p>
   <a href="/bun-cha">Bún Chả</a>
   <a href="/thuoc-lao">Thuốc Lào</a>
   <a href="/tra-da">Trà Đá</a>
