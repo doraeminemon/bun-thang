de39622ffa7d85e52ad2fbd21a14863079d4b6de
Tyson Do <doraeminemon@gmail.com>
2025-03-04 23:14:30 +0700

    Update phở gà and trà đá page.

---
+++ src/routes/pho-ga/+page.svelte
@@ -0,0 +1,3 @@
+<main>
+  <p>Món phở rất thanh nếu so sánh với phở bò. Nước dùng ngọt thanh, ăn dễ trôi, rất phù hợp với người ốm. </p>
+</main>
--- src/routes/tra-da/+page.svelte
+++ src/routes/tra-da/+page.svelte
@@ -1,2 +1,3 @@
 <main>
+  <p>Trà mạn, một thức quà vô cùng dân dã mà đâu đâu trên đất Việt đều có. Phù hợp để dùng cùng bữa ăn, sau bữa ăn hay kèm vài hơi thuốc lào</p>
 </main>
