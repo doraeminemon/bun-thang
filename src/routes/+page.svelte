7cffb07f49c5283981486b34f02cc19354581f28
Tyson Do <doraeminemon@gmail.com>
2025-03-05 13:04:17 +0700

    Add list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,12 @@
 <section class="m-4 min-h-screen flex flex-col">
   <h1>Thuốc Lào Blog</h1>
   <p class="py-4">Khoảnh đất nơi góp nhặt những ý nghĩ của một kẻ yêu Hà Nội.</p>
-  <a href="/bun-cha">Bún Chả</a>
-  <a href="/thuoc-lao">Thuốc Lào</a>
-  <a href="/tra-da">Trà Đá</a>
-  <a href="/bun-thang">Bún Thang</a>
-  <a href="/pho-ga">Phở Gà</a>
-  <a href="/che-lam">Chè Lam</a>
+  <ul class="list-disc pl-4">
+    <li><a href="/bun-cha">Bún Chả</a></li>
+    <li><a href="/thuoc-lao">Thuốc Lào</a></li>
+    <li><a href="/tra-da">Trà Đá</a></li>
+    <li><a href="/bun-thang">Bún Thang</a></li>
+    <li><a href="/pho-ga">Phở Gà</a></li>
+    <li><a href="/che-lam">Chè Lam</a></li>
+  </ul>
 </section>
