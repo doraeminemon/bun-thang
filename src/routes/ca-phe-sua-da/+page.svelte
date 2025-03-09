aa5b16f791c407b8264c41c689c9fb9d43ea087b
Tyson Do <doraeminemon@gmail.com>
2025-03-09 14:00:23 +0700

    feat(content): Chỉnh sửa câu từ của bài viết về cà phê sữa đá.

--- src/routes/ca-phe-sua-da/+page.svelte
+++ src/routes/ca-phe-sua-da/+page.svelte
@@ -2,11 +2,12 @@
   <h1>Cà phê sữa đá</h1>
   <p>
     Đã là một người Hà Nội / Việt Nam, hẳn không thể thiếu ly cà phê sữa đá mỗi buổi sáng.
-    Hơi tội lỗi xíu, nhưng tôi khá mê vị cà phê sữa đá ( Dolce Espresso của Starbucks ).
-    Ly cà phê của người Việt nổi tiếng.
+    Hơi tội lỗi xíu, nhưng tôi khá mê vị cà phê sữa đá Dolce Espresso của Starbucks.
+    Sau đây tôi xin viết về sự khác biệt giữa ly cà phê sữa đá gốc với ly Dolce Espresso
+    của Starbucks.
     <br/>
     <br/>
-    Vị đậm, nặng, như cá tính của người Việt vậy.
+    Ly cà phê của người Việt nổi tiếng với vị đậm, nặng, như cá tính của người Việt vậy.
     Một ly cà phê sữa đá mỗi buổi sáng sẽ giúp bạn tỉnh táo trong cả một ngày dài.
     Ly cà phê sữa đá của Starbucks thì có vẻ lại có chút khôn khéo hơn - vị cà phê cũng
     đậm nhưng không quá đắng, mà thay vào đó họ tạo lại độ nồng của ly cà phê nhờ việc
@@ -18,6 +19,9 @@
     <br/>
     Cũng chính vì nồng độ caffeine cao của ly cà phê sữa đá của Việt Nam từ hạt robusta
     buộc người uống phải uống thật chậm, và ăn một chút điểm tâm sáng nếu bạn không muốn
-    điểm dừng chân tiếp theo của bạn là toilet.
+    điểm dừng chân tiếp theo của bạn là toilet. Tôi vẫn mê cái sự khôn khéo của ly
+    Starbucks là vì vậy: người ta có thể vẫn uống thoải mái lúc nào mình muốn, với tốc
+    độ của mình muốn khiến ly cà phê Starbucks thơm ngon và đỡ kén người uống hơn ly
+    cà phê sữa đá.
   </p>
 </div>
