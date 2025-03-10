0b73f1eafc3c4512c785e7cda1cf95db64029062
Tyson Do <doraeminemon@gmail.com>
2025-03-10 20:53:41 +0700

    feat(refactor): Move icon to be an element, move every style to layer base

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,18 @@
 	<title>Thuốc Lào Blog</title>
 	<meta name="description" content="Cái xô chứa những suy nghĩ lắng đọng của một kẻ yêu Hà Nội." />
 </svelte:head>
+<script>
+  import BookIcon from '$lib/assets/icons/book-icon.svelte'
+</script>
 <section class="m-4 min-h-screen flex flex-col">
   <p class="py-4">Cái xô chứa những suy nghĩ lắng đọng của một kẻ yêu Hà Nội.</p>
   <ul class="mb-4 list-book list-inside">
-    <li><a href="/bun-cha">Bún Chả</a></li>
-    <li><a href="/thuoc-lao">Thuốc Lào</a></li>
-    <li><a href="/tra-da">Trà Đá</a></li>
-    <li><a href="/bun-thang">Bún Thang</a></li>
-    <li><a href="/pho-ga">Phở Gà</a></li>
-    <li><a href="/che-lam">Chè Lam</a></li>
-    <li><a href="/ca-phe-sua-da">Cà Phê Sữa Đá</a></li>
+    <li><a href="/bun-cha"><BookIcon/>Bún Chả</a></li>
+    <li><a href="/thuoc-lao"><BookIcon/>Thuốc Lào</a></li>
+    <li><a href="/tra-da"><BookIcon/>Trà Đá</a></li>
+    <li><a href="/bun-thang"><BookIcon/>Bún Thang</a></li>
+    <li><a href="/pho-ga"><BookIcon/>Phở Gà</a></li>
+    <li><a href="/che-lam"><BookIcon/>Chè Lam</a></li>
+    <li><a href="/ca-phe-sua-da"><BookIcon/>Cà Phê Sữa Đá</a></li>
   </ul>
 </section>
