83bc37a399e57d72bca3971a48a212f58ade83d4
Tyson Do <doraeminemon@gmail.com>
2025-03-10 20:24:09 +0700

    feat(style): Refactor layout, add icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,8 @@
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import { dev } from '$app/environment';
 	import { onMount } from 'svelte';
+	import CashIcon from '$lib/assets/icons/cash-icon.svelte';
+	import AtIcon from '$lib/assets/icons/at-icon.svelte';
 	onMount(() => {
 		injectSpeedInsights();
 		injectAnalytics({ mode: dev ? 'development': 'production' });
@@ -16,15 +18,18 @@
 	<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
 </svelte:head>
 
-<nav class="m-4 sticky top-0 left-0 bg-white">
+<nav>
 	<a href="/">
 		<h1>Thuốc Lào Blog</h1>
 	</a>
 </nav>
-<main class="max-w-[700px]">
+<main>
 	{@render children()}
 </main>
-<footer class="sticky bottom-0 left-0 w-full bg-white px-4 py-4">
-  <p>Viết bởi <a href="mailto:dodinhthyson@gmail.com" rel="me">Đỗ Đình Thy Sơn</a></p>
-  <a href="/hom-cong-duc">Hòm công đức</a>
+<footer>
+  <p>Viết bởi <a href="mailto:dodinhthyson@gmail.com" rel="me"><AtIcon />Đỗ Đình Thy Sơn</a></p>
+  <a href="/hom-cong-duc">
+		<CashIcon />
+		 Hòm công đức
+	</a>
 </footer>
