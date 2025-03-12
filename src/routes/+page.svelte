2bd06ad035042f4c2352fa3916842e243af62723
Tyson Do <doraeminemon@gmail.com>
2025-03-12 11:29:12 +0700

    feat(refactor): Move ListItem to be a component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,17 +3,17 @@
 	<meta name="description" content="Cái xô chứa những suy nghĩ lắng đọng của một kẻ yêu Hà Nội." />
 </svelte:head>
 <script>
-  import BookIcon from '$lib/assets/icons/book-icon.svelte'
+  import LinkItem from '$components/list-item.svelte'
 </script>
 <section class="m-4 min-h-screen flex flex-col">
   <p class="py-4">Cái xô chứa những suy nghĩ lắng đọng của một kẻ yêu Hà Nội.</p>
   <ul class="mb-4 list-book list-inside">
-    <li><a href="/bun-cha"><BookIcon/>Bún Chả</a></li>
-    <li><a href="/thuoc-lao"><BookIcon/>Thuốc Lào</a></li>
-    <li><a href="/tra-da"><BookIcon/>Trà Đá</a></li>
-    <li><a href="/bun-thang"><BookIcon/>Bún Thang</a></li>
-    <li><a href="/pho-ga"><BookIcon/>Phở Gà</a></li>
-    <li><a href="/che-lam"><BookIcon/>Chè Lam</a></li>
-    <li><a href="/ca-phe-sua-da"><BookIcon/>Cà Phê Sữa Đá</a></li>
+    <LinkItem href="/bun-cha">Bún Chả</LinkItem>
+    <LinkItem href="/thuoc-lao">Thuốc Lào</LinkItem>
+    <LinkItem href="/tra-da">Trà Đá</LinkItem>
+    <LinkItem href="/bun-thang">Bún Thang</LinkItem>
+    <LinkItem href="/pho-ga">Phở Gà</LinkItem>
+    <LinkItem href="/che-lam">Chè Lam</LinkItem>
+    <LinkItem href="/ca-phe-sua-da">Cà Phê Sữa Đá</LinkItem>
   </ul>
 </section>
