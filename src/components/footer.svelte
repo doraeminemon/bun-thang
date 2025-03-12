36182a8d1ab9cdb593f929f1315010a6df6822b2
Tyson Do <doraeminemon@gmail.com>
2025-03-12 11:30:57 +0700

    feat(style): Define footer to be of type flex col

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -2,7 +2,7 @@
 	import CashIcon from '$lib/assets/icons/cash-icon.svelte';
 	import AtIcon from '$lib/assets/icons/at-icon.svelte';
 </script>
-<footer>
+<footer class="flex flex-col">
   <a href="mailto:dodinhthyson@gmail.com" rel="me"><AtIcon />Đỗ Đình Thy Sơn</a>
   <a href="/hom-cong-duc">
 		<CashIcon />
