99eee7cd57491d10241658dcb2a889f856c044c4
Tyson Do <doraeminemon@gmail.com>
2025-03-12 12:43:55 +0700

    feat(style): move navbar and footer styling to component

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -9,3 +9,16 @@
 		 Hòm công đức
 	</a>
 </footer>
+<style lang="postcss">
+	@reference "tailwindcss";
+
+	footer {
+		@apply sticky bottom-0 left-0 w-full bg-white px-4 py-4;
+	}
+
+	@media (prefers-color-scheme: dark) {
+		footer {
+			@apply bg-blue-950;
+		}
+	}
+</style>
--- src/components/nav-bar.svelte
+++ src/components/nav-bar.svelte
@@ -3,3 +3,16 @@
 		<h1>Thuốc Lào Blog</h1>
 	</a>
 </nav>
+<style lang="postcss">
+	@reference "tailwindcss";
+
+	nav {
+		@apply sticky top-0 left-0 m-4 bg-white;
+	}
+
+	@media (prefers-color-scheme: dark) {
+		nav {
+			@apply bg-blue-950;
+		}
+	}
+</style>
