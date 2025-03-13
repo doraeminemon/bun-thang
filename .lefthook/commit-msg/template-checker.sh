c374dbf40a0e6ebbb8b73df19f69c552db7a5e3d
Tyson Do <doraeminemon@gmail.com>
2025-03-13 20:22:58 +0700

    feat(a11y): Add accessibility support and color change

--- .lefthook/commit-msg/template-checker.sh
+++ .lefthook/commit-msg/template-checker.sh
@@ -1,6 +1,6 @@
 INPUT_FILE=$1
 START_LINE=`head -n1 $INPUT_FILE`
-PATTERN="(feat|fix)\([a-z\-]+\):.+"
+PATTERN="(feat|fix)\([a-z0-9\-]+\):.+"
 if ! [[ "$START_LINE" =~ $PATTERN ]]; then
   echo "Bad commit message, see example: feat(foo): some text"
   exit 1
