91bc6d12293794f4d0909cb58db808e00111e91f
Tyson Do <doraeminemon@gmail.com>
2025-03-13 11:28:14 +0700

    test-commit-checker

---
+++ .lefthook/commit-msg/template-checker.sh
@@ -0,0 +1,7 @@
+INPUT_FILE=$1
+START_LINE=`head -n1 $INPUT_FILE`
+PATTERN="^[feat|fix]\(\w+\):.+: "
+if ! [[ "$START_LINE" =~ $PATTERN ]]; then
+  echo "Bad commit message, see example: feat(foo): some text"
+  exit 1
+fi
