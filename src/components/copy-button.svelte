e5d1fc8ce7d98e193bf4b998f67df46707760e2b
Tyson Do <doraeminemon@gmail.com>
2025-03-14 18:52:33 +0700

    feat(style): Add layout for donation page.

--- src/components/copy-button.svelte
+++ src/components/copy-button.svelte
@@ -11,7 +11,7 @@
     }, 500)
   }
 </script>
-<button class="inline" onclick={copyAddress}>
+<button onclick={copyAddress}>
   {#if isCopied}
     <CopyIcon />
   {:else}
@@ -22,6 +22,6 @@
   @reference "$styles/app.css";
 
   button {
-		@apply inline-flex items-center justify-center gap-x-2 rounded-lg border border-gray-200 bg-white p-2 text-center text-sm font-medium text-gray-800 shadow-2xs hover:bg-gray-50 focus:bg-gray-50 focus:outline-hidden disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-neutral-800 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700;
+		@apply flex w-8 items-center justify-center gap-x-2 rounded-lg border border-gray-200 bg-white p-2 text-center text-sm font-medium text-gray-800 shadow-2xs hover:bg-gray-50 focus:bg-gray-50 focus:outline-hidden disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-neutral-800 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700;
 	}
 </style>
--- src/routes/hom-cong-duc/+page.svelte
+++ src/routes/hom-cong-duc/+page.svelte
@@ -8,12 +8,12 @@
 <div class="content">
   <h1>Hòm công đức</h1>
   <p class="mb-4">Nếu thấy blog hay, bạn có thể thoải mái gửi tiền vào 1 trong 2 địa chỉ sau:</p>
-  <div class="grid grid-cols-[7rem_24rem_2rem]">
+  <div class="grid md:grid-cols-[7rem_24rem_2rem] grid-cols-1 gap-2">
     <p class="font-mono flex items-center">Techcombank: </p>
     <p class="font-mono flex items-center">DODINHTHYSON</p>
     <CopyButton content="DODINHTHYSON"/>
     <p class="font-mono flex items-center">Crypto: </p>
-    <p class="font-mono flex items-center">0xA24142bBFb0590511f26cE8f4658E70390dB8C0b</p>
+    <p class="font-mono flex items-center truncate">0xA24142bBFb0590511f26cE8f4658E70390dB8C0b</p>
     <CopyButton content="0xA24142bBFb0590511f26cE8f4658E70390dB8C0b"/>
   </div>
 </div>
