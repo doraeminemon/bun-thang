cf09bb5df959abab794f50a2a438dad023773451
Tyson Do <doraeminemon@gmail.com>
2025-03-24 18:23:12 +0700

    feat(content): Add a page for a new song by uncle Văn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,22 @@
+<script>
+	import LinkItem from '$components/list-item.svelte';
+</script>
+
 <svelte:head>
 	<title>Thuốc Lào Blog</title>
 	<meta name="description" content="Cái xô chứa những suy nghĩ lắng đọng của một kẻ yêu Hà Nội." />
 </svelte:head>
-<script>
-  import LinkItem from '$components/list-item.svelte'
-</script>
 <section class="mx-4 flex flex-col gap-y-4">
-  <p>Cái xô chứa những suy nghĩ lắng đọng của một kẻ yêu Hà Nội.</p>
-  <ul class="mb-4 list-book list-inside">
-    <LinkItem href="/bun-cha">Bún Chả</LinkItem>
-    <LinkItem href="/thuoc-lao">Thuốc Lào</LinkItem>
-    <LinkItem href="/tra-da">Trà Đá</LinkItem>
-    <LinkItem href="/bun-thang">Bún Thang</LinkItem>
-    <LinkItem href="/pho-ga">Phở Gà</LinkItem>
-    <LinkItem href="/pho-bo">Phở Bò</LinkItem>
-    <LinkItem href="/che-lam">Chè Lam</LinkItem>
-    <LinkItem href="/ca-phe-sua-da">Cà Phê Sữa Đá</LinkItem>
-  </ul>
+	<p>Cái xô chứa những suy nghĩ lắng đọng của một kẻ yêu Hà Nội.</p>
+	<ul class="list-book mb-4 list-inside">
+		<LinkItem href="/bun-cha">Bún Chả</LinkItem>
+		<LinkItem href="/thuoc-lao">Thuốc Lào</LinkItem>
+		<LinkItem href="/tra-da">Trà Đá</LinkItem>
+		<LinkItem href="/bun-thang">Bún Thang</LinkItem>
+		<LinkItem href="/pho-ga">Phở Gà</LinkItem>
+		<LinkItem href="/pho-bo">Phở Bò</LinkItem>
+		<LinkItem href="/che-lam">Chè Lam</LinkItem>
+		<LinkItem href="/ca-phe-sua-da">Cà Phê Sữa Đá</LinkItem>
+		<LinkItem href="/ha-noi-khoanh-khac">Hà Nội Khoảnh Khắc</LinkItem>
+	</ul>
 </section>
---
+++ src/routes/ha-noi-khoanh-khac/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>Thuốc Lào Blog - Hà Nội Khoảnh Khắc</title>
+	<meta name="description" content="Một ca khúc mang tên Hà Nội Khoảnh Khắc" />
+</svelte:head>
+<div class="content">
+	<h1>Hà Nội Khoảnh Khắc</h1>
+	<figure>
+		<figcaption>Bài Hát Hà Nội Khoảnh Khắc</figcaption>
+		<audio controls src="/ha-noi-khoanh-khac.mp3"></audio>
+		<a href="/ha-noi-khoanh-khac.mp3"> Tải về </a>
+	</figure>
+	<p>Sáng tác bởi chú Văn</p>
+</div>
