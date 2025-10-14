8e57c27663977514f6480533a2f7ba4775b76d7e
Tyson Do <doraeminemon@gmail.com>
2025-10-15 00:26:48 +0700

    Update README.md

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-vercel';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
