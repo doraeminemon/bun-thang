5d9fad01121ec2838c399a6fba20549f11c24728
FinnNguyen <Finn.Nguyen@cebupacificair.com>
2026-01-22 16:15:41 +0700

    🎨 Fix some formatting problem

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,25 @@
 <script>
 	import '$styles/app.css';
 	let { children } = $props();
-	import { injectAnalytics } from '@vercel/analytics/sveltekit'
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import { dev } from '$app/environment';
 	import { onMount } from 'svelte';
 	onMount(() => {
 		injectSpeedInsights();
-		injectAnalytics({ mode: dev ? 'development': 'production' });
-	})
+		injectAnalytics({ mode: dev ? 'development' : 'production' });
+	});
 	import NavBar from '$components/nav-bar.svelte';
 	import Footer from '$components/footer.svelte';
 </script>
+
 <svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
-	<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+		rel="stylesheet"
+	/>
 </svelte:head>
 
 <NavBar />
@@ -23,10 +27,11 @@
 	{@render children()}
 </main>
 <Footer />
+
 <style lang="postcss">
 	@reference "tailwindcss";
 
 	main {
-		@apply min-h-screen max-w-[700px];
+		@apply min-h-screen max-w-175;
 	}
 </style>
